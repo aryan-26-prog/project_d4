eff9c57c69d5e8df3c0577348352ab13eb99d406
Aryan Dhiman <aryandhiman2605@gmail.com>
2026-01-06 04:08:03 +0530

    first commit

---
+++ routes/confessionRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { getConfession, createConfession, likeConfession, reactToConfession } = require('../controllers/confessionController');
+const Confession = require("../models/confession");
+
+const confessionRouter = express.Router();
+
+//This router is used to get all the confessions
+confessionRouter.get("/", getConfession);
+
+//This router is used to post all the confessions
+confessionRouter.post("/", createConfession);
+
+//This router is used to like a confession
+confessionRouter.patch("/:id/like", likeConfession); 
+
+//This router is used to emojis
+confessionRouter.patch("/:id/react/:type", reactToConfession);
+
+//This router is used to sort the trending one
+confessionRouter.get("/trending", async (req, res) => {
+  const trending = await Confession.find()
+    .sort({ likes: -1 })
+    .limit(20);
+
+  res.json(trending);
+});
+
+
+module.exports = confessionRouter;
